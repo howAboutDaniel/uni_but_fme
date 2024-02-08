4ab83ddccdb28370bd0fac42a4a8abc69f6f996d
Dlugoš Daniel (217099) <217099@vutbr.cz>
2024-02-08 10:32:39 +0100

    nedotknuty skript na 1. cvičenie

---
+++ cpp_1.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def f(x: np.ndarray):
+    return np.sum(x**2+4*x-6)
+
+
+def rndwalk(lb, ub, dim, objfunc):
+    """
+        lb, ub - spodni a horni meze
+        dim - pocet dimenzi
+        objfunc - ucelova funkce, musi byt Callable
+    """
+    # TODO: inicializace; nejlepsi reseni je nutno si pamatovat
+    best_x = None
+    best_fx = None
+    # TODO: aktualni reseni
+    x: np.ndarray = None
+
+    # TODO: max velikost kroku z rozsahu intervalu
+
+    # TODO: iterace algoritmu
+    #      krok
+    #      kontrola mezi
+    #      ulozeni nejelpsiho
+    
+    return best_x, best_fx
+
+
+def main():
+    # rndwalk(lb=2, ub=4.5, dim=2, objfunc=f)
+    x, fx = rndwalk(2, 4.5, 10, f)
+    print('x:', x)
+    print('f(x):', fx)
+
+
+if __name__ == '__main__':
+    main()
