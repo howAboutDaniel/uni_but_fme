f03f36bd34063c0bc76c3c0339925cdbf3b12b24
217099 <daniel.dlugos@me.com>
2024-03-07 10:42:55 +0100

    1st Chunk

---
+++ cv05_regression_ann.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# fc = @(x) 2*sin(x*5+8);
+def fc(x: np.ndarray):
+    return 2*np.sin(x*5+8)
+
+N = 140
+x = np.linspace(-2, 3, 1000)
+xt = np.linspace(-2, 3, N)
+yt = fx(x) + (randn(N)/2.5)
+
+
+plt.plot(x, fx(x), 'b-', xt, yt, 'ro')
+plt.show
